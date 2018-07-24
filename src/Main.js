4bd30f1864d4cbd7832e17fd334c963ee3c1e9aa
wen <wenwen820906@gmail.com>
2018-07-24 18:00:37 +0800

    add background img of main content

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+import './Main.css';
+
+const Main = () => {
+    return(
+        <div id="main" className="main" >
+       
+        <p>Canada</p>
+        <p>The First Time </p>
+        <Button bsStyle="primary" >More</Button>
+        </div>
+
+    )
+};
+
+export default Main ;
