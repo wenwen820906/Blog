bf1c6dc7c0e0dd78edc2c219f4abce096cd61891
wen <wenwen820906@gmail.com>
2018-07-31 21:21:03 +0800

    add navbar

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import {Nav,Navbar,NavItem} from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => { 
     return (
-     <div className="button-group">
-        <Button bsStyle="primary" >Home</Button>
-        &nbsp;
-        <Button bsStyle="primary">Gallery</Button>
-        <p>Follow Me To Travel Around CANADA</p>
+     <div>
+       <Navbar inverse collapseOnSelect>
+       
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="#brand">My Trip</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+
+        <Navbar.Collapse>
+            <Nav>
+                <NavItem eventKey={1} href="#">
+                     Home
+                </NavItem>
+                 <NavItem eventKey={2} href="#">
+                     Gallery
+                 </NavItem>
+             </Nav>
+        </Navbar.Collapse>
+        
+        </Navbar>
+        <div className = 'button-group' >
+            <h1>Follow Me To Travel Around CANADA</h1>
+        </div>
      </div>   
     )
 };
